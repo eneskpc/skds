fb50eb461e6f057da1b3ee77c186897bdff91912
rahmanbesir <elrahmanbesir@gmail.com>
2017-05-05 22:40:52 +0300

    örnek jsp yönlendirme

---
+++ src/model/Request.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Request {
+	
+	private String detail;
+
+	public Request(String detail) {
+		super();
+		this.detail = detail;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+	
+	
+
+}
--- src/ornekPaket/OrnekServlet.java
+++ src/ornekPaket/OrnekServlet.java
@@ -2,20 +2,35 @@ package ornekPaket;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Request;
+
 public class OrnekServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter pw = resp.getWriter();
 		
-		pw.print("<h1>Hi EveryOne!</h1>"); //yükleme testi
+		ArrayList<Request> talepler = new ArrayList<>();
 		
+		talepler.add(new Request("Hello"));
+		talepler.add(new Request("Naber"));
+		talepler.add(new Request("Java"));
+		talepler.add(new Request("Php"));
+		talepler.add(new Request("Laravel"));
+		
+		req.setAttribute("talepler", talepler);
+		
+		//dispatcher nesnesini servlet context objesinden oluþturuyoruz.
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/view/test.jsp");
+		dispatcher.forward(req, resp);
+		//daha sonra request nesnesine eklediðimiz attribute ile birlikte req ve resp i jsp ye yönlendiriyoruz.
 	}
 
 }
