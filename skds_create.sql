e04700e2c4d6e6a1bb5892a2eb8ec670a2f3476e
eneskpc <mr.enes.kapucu@gmail.com>
2017-05-14 14:40:28 +0300

    Veritabanı güncel sürüm, Bazı sütunların veritipi düzeltildi.

--- skds_create.sql
+++ skds_create.sql
@@ -1,128 +1,224 @@
--- Created by Vertabelo (http://vertabelo.com)
--- Last modification date: 2017-04-26 09:50:39.022
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 14 May 2017, 13:38:27
+-- Sunucu sürümü: 5.7.11
+-- PHP Sürümü: 7.0.4
 
--- tables
--- Table: Company
-CREATE TABLE Company (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
-    detail varchar(100) NOT NULL,
-    contactName varchar(25) NOT NULL,
-    contactPhone varchar(11) NOT NULL,
-    imageUrl int NOT NULL,
-    approved binary(1) NOT NULL,
-    User_id int NOT NULL,
-    CONSTRAINT Company_pk PRIMARY KEY (id)
-) COMMENT 'Şirket Bilgilerini tutar';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- Table: Customer
-CREATE TABLE Customer (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(25) NOT NULL,
-    gender varchar(1) NOT NULL,
-    birthYear int NOT NULL,
-    homeCity int NOT NULL,
-    User_id int NOT NULL,
-    CONSTRAINT Customer_pk PRIMARY KEY (id)
-);
 
--- Table: Messages
-CREATE TABLE Messages (
-    id int NOT NULL AUTO_INCREMENT,
-    sender_id int NOT NULL,
-    receiver_id int NOT NULL,
-    title varchar(20) NOT NULL,
-    detail varchar(100) NOT NULL,
-    previous int NOT NULL,
-    CONSTRAINT Messages_pk PRIMARY KEY (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Table: Reponse
-CREATE TABLE Reponse (
-    id int NOT NULL AUTO_INCREMENT,
-    message text NOT NULL,
-    previous int NOT NULL,
-    Request_id int NOT NULL,
-    User_id int NOT NULL,
-    CONSTRAINT Reponse_pk PRIMARY KEY (id)
-);
+--
+-- Veritabanı: `skds`
+--
 
--- Table: Request
-CREATE TABLE Request (
-    id int NOT NULL AUTO_INCREMENT,
-    title varchar(50) NOT NULL,
-    date date NOT NULL,
-    detail text NOT NULL,
-    Company_id int NOT NULL,
-    Customer_id int NOT NULL,
-    Staff_id int NOT NULL,
-    CONSTRAINT Request_pk PRIMARY KEY (id)
-);
+-- --------------------------------------------------------
 
--- Table: Staff
-CREATE TABLE Staff (
-    id int NOT NULL AUTO_INCREMENT,
-    name varchar(25) NOT NULL,
-    Company_id int NOT NULL,
-    User_id int NOT NULL,
-    CONSTRAINT Staff_pk PRIMARY KEY (id)
-);
+--
+-- Tablo için tablo yapısı `company`
+--
 
--- Table: User
-CREATE TABLE User (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(50) NOT NULL,
-    password varchar(10) NOT NULL,
-    name varchar(25) NOT NULL,
-    type int NOT NULL COMMENT '1-Müşteri / 2-Personel / 3-Şirket / 4-Webmaster',
-    CONSTRAINT User_pk PRIMARY KEY (id)
-) COMMENT 'Kullanıcı giriş bilgilerini ve kullanıcı tipini tutar
-';
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `detail` varchar(100) DEFAULT NULL,
+  `contactName` varchar(25) NOT NULL,
+  `contactPhone` varchar(11) DEFAULT NULL,
+  `imageUrl` text,
+  `approved` set('1','0') NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Şirket Bilgilerini tutar';
 
--- foreign keys
--- Reference: Company_User (table: Company)
-ALTER TABLE Company ADD CONSTRAINT Company_User FOREIGN KEY Company_User (User_id)
-    REFERENCES User (id);
+-- --------------------------------------------------------
 
--- Reference: Customer_User (table: Customer)
-ALTER TABLE Customer ADD CONSTRAINT Customer_User FOREIGN KEY Customer_User (User_id)
-    REFERENCES User (id);
+--
+-- Tablo için tablo yapısı `customer`
+--
 
--- Reference: Messages_User_Receiver (table: Messages)
-ALTER TABLE Messages ADD CONSTRAINT Messages_User_Receiver FOREIGN KEY Messages_User_Receiver (receiver_id)
-    REFERENCES User (id);
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `gender` varchar(1) DEFAULT NULL,
+  `birthYear` int(11) DEFAULT NULL,
+  `homeCity` int(11) DEFAULT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Reference: Messages_User_Sender (table: Messages)
-ALTER TABLE Messages ADD CONSTRAINT Messages_User_Sender FOREIGN KEY Messages_User_Sender (sender_id)
-    REFERENCES User (id);
+-- --------------------------------------------------------
 
--- Reference: Reponse_Request (table: Reponse)
-ALTER TABLE Reponse ADD CONSTRAINT Reponse_Request FOREIGN KEY Reponse_Request (Request_id)
-    REFERENCES Request (id);
+--
+-- Tablo için tablo yapısı `messages`
+--
 
--- Reference: Reponse_User (table: Reponse)
-ALTER TABLE Reponse ADD CONSTRAINT Reponse_User FOREIGN KEY Reponse_User (User_id)
-    REFERENCES User (id);
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `sender_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL,
+  `detail` varchar(100) NOT NULL,
+  `previous` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Reference: Request_User_Company (table: Request)
-ALTER TABLE Request ADD CONSTRAINT Request_User_Company FOREIGN KEY Request_User_Company (Customer_id)
-    REFERENCES User (id);
+-- --------------------------------------------------------
 
--- Reference: Request_User_Customer (table: Request)
-ALTER TABLE Request ADD CONSTRAINT Request_User_Customer FOREIGN KEY Request_User_Customer (Staff_id)
-    REFERENCES User (id);
+--
+-- Tablo için tablo yapısı `reponse`
+--
 
--- Reference: Request_User_Staff (table: Request)
-ALTER TABLE Request ADD CONSTRAINT Request_User_Staff FOREIGN KEY Request_User_Staff (Company_id)
-    REFERENCES User (id);
+CREATE TABLE `reponse` (
+  `id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `previous` int(11) NOT NULL,
+  `Request_id` int(11) NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Reference: Staff_User_Company (table: Staff)
-ALTER TABLE Staff ADD CONSTRAINT Staff_User_Company FOREIGN KEY Staff_User_Company (Company_id)
-    REFERENCES User (id);
+-- --------------------------------------------------------
 
--- Reference: Staff_User_Staff (table: Staff)
-ALTER TABLE Staff ADD CONSTRAINT Staff_User_Staff FOREIGN KEY Staff_User_Staff (User_id)
-    REFERENCES User (id);
+--
+-- Tablo için tablo yapısı `request`
+--
 
--- End of file.
+CREATE TABLE `request` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `detail` text NOT NULL,
+  `Company_id` int(11) NOT NULL,
+  `Customer_id` int(11) NOT NULL,
+  `Staff_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `Company_id` int(11) NOT NULL,
+  `User_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `type` int(11) NOT NULL COMMENT '1-Müşteri / 2-Personel / 3-Şirket / 4-Webmaster'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Kullanıcı giriş bilgilerini ve kullanıcı tipini tutar\n';
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Company_User` (`User_id`);
+
+--
+-- Tablo için indeksler `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Customer_User` (`User_id`);
+
+--
+-- Tablo için indeksler `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Messages_User_Receiver` (`receiver_id`),
+  ADD KEY `Messages_User_Sender` (`sender_id`);
+
+--
+-- Tablo için indeksler `reponse`
+--
+ALTER TABLE `reponse`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Reponse_Request` (`Request_id`),
+  ADD KEY `Reponse_User` (`User_id`);
+
+--
+-- Tablo için indeksler `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Request_User_Company` (`Customer_id`),
+  ADD KEY `Request_User_Customer` (`Staff_id`),
+  ADD KEY `Request_User_Staff` (`Company_id`);
+
+--
+-- Tablo için indeksler `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Staff_User_Company` (`Company_id`),
+  ADD KEY `Staff_User_Staff` (`User_id`);
+
+--
+-- Tablo için indeksler `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `company`
+--
+ALTER TABLE `company`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `reponse`
+--
+ALTER TABLE `reponse`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `request`
+--
+ALTER TABLE `request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Tablo için AUTO_INCREMENT değeri `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
